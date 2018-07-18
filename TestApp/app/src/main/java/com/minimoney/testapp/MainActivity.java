36e5765c127df7f18f5f3413cedbde881a3a947e
CMD <kovacslaa@gmail.com>
2018-07-19 01:45:36 +0200

    New empty project created, and did homework

---
+++ TestApp/app/src/main/java/com/minimoney/testapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.minimoney.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonTap(View v) {
+        Toast toastObj = Toast.makeText(getApplicationContext(), "Ouch!", Toast.LENGTH_LONG );
+        toastObj.show();
+    }
+}
+
+
